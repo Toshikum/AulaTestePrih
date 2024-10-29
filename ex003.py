ee43510fcd59c1e561c4015c72ea4fb8eb5cb2d5
pri <priiscila52@outlook.com>
2024-10-29 20:47:49 -0300

    arquivo ex2 ex3 add

---
+++ ex003.py
@@ -0,0 +1,9 @@
+#Crie um programa que leia dois númeors e mostre a soma entre elas.
+
+n1 = int(input('Digite um valor'))
+n2 = int(input('Digite um valor'))
+
+s= n1 + n2
+
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
+
